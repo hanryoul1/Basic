0bf0f4ab8db08a16f3b93138cd0f185a5e19f4ad
hanryoul1 <1228gksfbf@naver.com>
2022-01-07 19:33:13 +0900

    나머지

--- Step05 1차원 배열/05_04.py
+++ Step05 1차원 배열/05_04.py
@@ -7,6 +7,6 @@ for i in range(10):
 for j in range(10):
     result_list.append(num_list[j]%42)
 
-result_list = set(result_list)
-result_list = list(result_list)
-print(len(result_list))
+result_list = set(result_list) # set으로 변경하여 중복요소 제거
+result_list = list(result_list) # 다시 list로 변경
+print(len(result_list)) # 요소 개수 출력
