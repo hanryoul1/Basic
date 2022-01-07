5b6736fe229b9caae19ea515f40a35047bccb850
hanryoul1 <1228gksfbf@naver.com>
2022-01-08 00:40:11 +0900

    OX퀴즈

---
+++ Step05 1차원 배열/05_06.py
@@ -0,0 +1,14 @@
+cnt = int(input())
+
+for i in range(cnt):
+    a = input()
+    score = 0
+    sumscore = 0
+
+    for j in a:
+        if j == 'O':
+            score += 1
+        else:
+            score = 0
+        sumscore += score
+    print(sumscore)
