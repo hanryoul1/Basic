3f445814cd421c3e58dd02cbc014f7b7d9851c3c
hanryoul1 <1228gksfbf@naver.com>
2022-01-09 22:15:48 +0900

    알파벳 찾기

--- Step07 문자열/07_03.py
+++ Step07 문자열/07_03.py
@@ -5,4 +5,4 @@ for i in alphabet:
     if i in s:
         print(s.index(i), end =' ')
     else:
-        print(-1, end = ' ')
+        print(-1, end = ' ')  # 한칸씩 띄어서 반환하기 때문에 출력이후 한칸을 띄우는 end=' ' 사용
