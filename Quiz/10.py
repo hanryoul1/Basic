6e0ee9909664a6e63e4209315707a1a7688d672f
hanryoul1 <1228gksfbf@naver.com>
2022-10-03 15:27:59 +0900

    Quiz_10

---
+++ Quiz/10.py
@@ -0,0 +1,17 @@
+"""
+Quiz) 프로젝트 내에 나만의 시그니처를 남기는 모듈을 만드시오
+
+조건) 모듈 파일명은 byme.py 로 작성
+
+모듈 사용 예제)
+import byme
+byme.sign()
+
+출력 예제)
+이 프로그램은 한률에 의해 만들어졌습니다.
+이메일 : 1228gksfbf@khu.ac.kr
+인스타그램 : https://www.instagram.com/hryoul1/
+"""
+
+import byme 
+byme.sign()
---
+++ Quiz/byme.py
@@ -0,0 +1,5 @@
+
+def sign():
+    print("이 프로그램은 한률에 의해 만들어졌습니다.")
+    print("이메일 : 1228gksfbf@khu.ac.kr")
+    print("인스타그램 : https://www.instagram.com/hryoul1/")
