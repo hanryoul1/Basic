40e2a7149b4bb6d7c47859266308f6e704e66cde
hanryoul1 <1228gksfbf@khu.ac.kr>
2022-10-16 18:22:49 +0900

    통계학

--- Step09 정렬/09_05.py
+++ Step09 정렬/09_05.py
@@ -1,28 +1,39 @@
 import sys
+from collections import Counter
 
-lst = []
 N = int(input())
+input_num = [int(sys.stdin.readline()) for i in range(N)]
 
+"""
 for i in range(N):
     input_num = int(sys.stdin.readline())
     lst.append(input_num)
+"""
 
 # 산술평균
-a = round(sum(lst)/N)
+a = round(sum(input_num)/N)
 
 # 중앙값
-lst.sort()
-b = lst[N//2]
+input_num.sort()
+b = input_num[N//2]
 
 # 최빈값
+cnt = Counter(input_num).most_common(2) # 가장 개수가 많은 k개의 데이터를 리턴
 
+if len(cnt) > 1: # 데이터가 2개 이상일 경우
+    if cnt[0][1] == cnt[1][1]:
+        d = cnt[1][0]
+
+    else:
+        d = cnt[0][0]
+
+else:
+    d = cnt[0][0]
 
 # 범위
-c = lst[N-1] - lst[0]
+c = input_num[N-1] - input_num[0]
 
 print(a)
 print(b)
+print(d)
 print(c)
-
-
-
