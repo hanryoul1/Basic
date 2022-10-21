aa3a83034464c8aa9973a68e9429d50400d7a233
hanryoul1 <1228gksfbf@khu.ac.kr>
2022-10-21 22:00:42 +0900

    ACM 호텔

---
+++ Step08 기본 수학/08_05.py
@@ -0,0 +1,33 @@
+# Solution_MY
+import sys
+
+T = int(sys.stdin.readline())
+
+for i in range(T):
+    H, W, N = map(int, sys.stdin.readline().split())
+
+    Y = N % H
+    X = (W // Y) - 1
+    
+    if N % H == 0:
+        Y = N // H
+        X = H
+
+    print(f"{Y}0{X}")
+
+# Solution
+import sys
+
+T = int(sys.stdin.readline())
+
+for i in range(T):
+    H, W, N = map(int, sys.stdin.readline().split())
+
+    Y = N % H
+    X = N // H + 1
+    
+    if N % H == 0:
+        Y = H
+        X = N // H
+        
+    print("%d%02d"%(Y, X))
