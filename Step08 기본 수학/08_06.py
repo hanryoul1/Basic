47c0c7b1c3032fb2a9059e063d044b759a7b88bb
hanryoul1 <1228gksfbf@khu.ac.kr>
2022-10-22 18:35:47 +0900

    부녀회장이 될테야

--- Step08 기본 수학/08_06.py
+++ Step08 기본 수학/08_06.py
@@ -1,5 +1,12 @@
-import sys
-T = int(sys.stdin.readline())
+T = int(input()) #테스트 케이스
+
 for i in range(T):
-    k = int(sys.stdin.readline())
-    n = int(sys.stdin.readline())
+    k = int(input()) #층
+    n = int(input()) #호
+    zero_User = list(range(1, n+1))
+    
+    for x in range(k):
+        for y in range(1, n):
+            zero_User[y] += zero_User[y-1] #리스트 안에서 1층 인원수로 교체
+    
+    print(zero_User[-1]) #리스트의 제일 마지막 출력
