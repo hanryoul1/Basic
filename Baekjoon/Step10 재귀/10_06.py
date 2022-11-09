c7a39cc8699c49bacbfd5bb548affc1b179d65b7
hanryoul1 <1228gksfbf@khu.ac.kr>
2022-11-09 19:05:55 +0900

    Baekjoon

---
+++ Baekjoon/Step10 재귀/10_06.py
@@ -0,0 +1,16 @@
+def hanoi(n, start, mid, finish):
+    if n == 1:
+        print(a, c)
+        
+
+
+if __name__ == "__main__":
+    n = int(input())
+    cnt = 1
+    for i in range(n-1):
+        cnt = cnt *2 + 1
+    print(cnt)
+    hanoi(n, 1, 2, 3)
+
+# https://velog.io/@rmswjdtn/%EC%9E%AC%EA%B7%80-BaekJoon-11729-%ED%95%98%EB%85%B8%EC%9D%B4-%ED%83%91-%EC%9D%B4%EB%8F%99%EC%88%9C%EC%84%9C
+# https://simsim231.tistory.com/133
