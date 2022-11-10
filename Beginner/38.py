a1873866dff898285f9206a0c9e04620da15ff3d
hanryoul1 <1228gksfbf@khu.ac.kr>
2022-11-10 13:15:58 +0900

    파이썬 문자열

---
+++ Beginner/38.py
@@ -0,0 +1,4 @@
+상장주식수 = "5,969,782,550"
+컴마제거 = 상장주식수.replace(",", "")
+result = int(컴마제거)
+print(result, type(result))
---
+++ Beginner/40.py
@@ -0,0 +1,4 @@
+data = "   삼성전자    "
+result = data.strip()
+print(result)
+# 원본 문자열은 그대로 유지되고 공백이 제거된 새로운 문자열이 반환
