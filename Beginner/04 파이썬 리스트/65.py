bbde3c3c6804f5d2695fa249cac0da250a38dc5f
hanryoul1 <1228gksfbf@khu.ac.kr>
2022-11-13 18:16:09 +0900

    파이썬 리스트

---
+++ Beginner/04 파이썬 리스트/65.py
@@ -0,0 +1,3 @@
+interest = ['삼성전자', 'LG전자', 'Naver']
+print(interest[0::2]) # ['삼성전자', 'Naver']
+print(interest[0], interest[2]) # 삼성전자 Naver
---
+++ Beginner/04 파이썬 리스트/70.py
@@ -0,0 +1,9 @@
+# sort / 원본변형
+data = [2, 4, 3, 1, 5, 10, 9]
+data.sort()
+print(data)
+
+# sorted / 원본유지
+data = [2, 4, 3, 1, 5, 10, 9]
+data2 = sorted(data)
+print(data2)
