f40b704d5b3506df2ab4b86829884c7145df0707
hanryoul1 <1228gksfbf@khu.ac.kr>
2022-11-14 20:44:57 +0900

    파이썬 딕셔너리

---
+++ Beginner/06 파이썬 딕셔너리/95.py
@@ -0,0 +1,5 @@
+icecream = {'탱크보이': 1200, '폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+key_data = list(icecream.keys())
+# 파이썬 3.0 이상 버전부터는 리스트로 키를 가져오고 싶다면, list 함수로 다시 한번 감싸 줘야 함
+
+print(key_data, type(key_data))
---
+++ Beginner/06 파이썬 딕셔너리/97.py
@@ -0,0 +1,7 @@
+icecream = {'탱크보이': 1200, '폴라포': 1200, '빵빠레': 1800, '월드콘': 1500, '메로나': 1000}
+value_data = list(icecream.values())
+
+print("icecream.values() ->", icecream.values())
+print("list(icecream.values()) ->", value_data)
+print("sum(icecream.values()) ->", sum(icecream.values()))
+print("sum(value_data) ->", sum(value_data))
