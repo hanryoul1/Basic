00f8f974f4e7e3056bcbe83e5673e8aa3d67eff1
hanryoul1 <1228gksfbf@khu.ac.kr>
2022-11-15 13:22:07 +0900

    파이썬 분기문(101_110)

---
+++ Beginner/07 파이썬 분기문/101_110.py
@@ -0,0 +1,48 @@
+# 101.py
+# 'bool'타입 = True , False
+
+# 102.py
+print(3 == 5) 
+
+# 103.py
+print(3 < 5)
+
+# 104.py
+x = 4
+print(1 < x < 5)
+
+# 105.py
+print((3 == 3) and (4 != 3)) # True and True => True
+
+# 106.py
+print(3 >= 4) # (=>) X / (<=) O 
+
+# 107.py
+if 4 < 3:
+    print("Hello World")
+
+# 108.py
+if 4 < 3:
+    print("Hello World")
+else:
+    print("Hi, there.")
+
+# 109.py
+if True:
+    print("1")
+    print("2")
+else:
+    print("3")
+print("4")
+
+# 110.py
+if True:
+    if False:
+        print("1")
+        print("2")
+    else:
+        print("3")
+
+else:
+    print("4")
+print("5")
