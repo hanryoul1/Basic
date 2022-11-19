8d32705537c20d6e320260b7792aebaa2b51f891
hanryoul1 <1228gksfbf@khu.ac.kr>
2022-11-20 01:17:52 +0900

    파이썬 반복문(141_150)

---
+++ Beginner/08 파이썬 반복문/141_150.py
@@ -0,0 +1,45 @@
+# 141.py
+lst1 = [100, 200, 300]
+for i in lst1:
+    print(i + 10)
+
+# 142.py
+lst2 = ["김밥", "라면", "튀김"]
+for j in lst2:
+    print("오늘의 메뉴: " + j)
+
+# 143.py
+lst3 = ["Sk하이닉스", "Samsung", "LG"]
+for k in lst3:
+    print(len(k))
+
+# 144.py
+lst4 = ["dog", "cat", "parrot"]
+for h in lst4:
+    print(h, len(h))
+
+# 145.py
+for l in lst4:
+    print(l[0])
+
+# 146.py
+lst6 = [1, 2, 3]
+for m in lst6:
+    print("3 x", m)
+
+# 147.py
+for n in lst6:
+    print("3 x", 3*n)
+
+# 148.py
+lst8 = ["가", "나", "다", "라"]
+for o in lst8[1:]:
+    print(o)
+
+# 149.py
+for p in lst8[0::2]:
+    print(p)
+
+# 150.py
+for q in lst8[::-1]:
+    print(q)
