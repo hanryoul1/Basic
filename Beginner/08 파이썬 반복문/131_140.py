92d3d904237a68cee5ca3f990d8ebf6bcb2a41f7
hanryoul1 <1228gksfbf@khu.ac.kr>
2022-11-19 19:57:53 +0900

    파이썬 반복문(131_140)

---
+++ Beginner/08 파이썬 반복문/131_140.py
@@ -0,0 +1,40 @@
+# 131.py
+fruit = ["사과", "귤", "수박"]
+for n in fruit:
+    print(n)
+
+# 132.py
+과일 = ["사과", "귤", "수박"]
+for 변수 in 과일:
+  print("#####")
+
+# 133.py
+for i in ["A", "B", "C"]:
+    print(i)
+    # "변수 바인딩(라인 1) -> 변수 출력 (라인 2)" 
+
+# 134.py
+for j in ["A", "B", "C"]:
+    print("출력:", j)
+
+# 135.py
+for k in ["A", "B", "C"]:
+    k = k.lower()
+    print("출력:", k)
+
+# 136.py & 137.py
+for h in [10, 20, 30]:
+    print(h)
+
+# 138.py
+for l in [10, 20, 30]:
+    print(l, "\n-------")
+
+# 139.py
+print("++++")
+for m in [10, 20, 30]:
+    print(m)
+
+# 140.py
+for n in range(4):
+    print("-------")
